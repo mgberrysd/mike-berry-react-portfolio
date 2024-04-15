8cba6b84db9c664a5853ca132c22f87652ef59d3
mgberrysd <mgberrysd@gmail.com>
2024-04-15 12:16:57 -0700

    new project

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
     <article className="container pt-4">
       <img src="../assets/images/test_image.jpg"></img>
       <p>
-        Newly minted full stack web developer. Front end fundementals in HTML, CSS, and JavaScript/TypeScript. Expereience with Bottstrap, Handlebars.js, and ReactJS. Back end experience with NodeJS, Express, Apollo/graphQL, MySQL/Sequelize, and MongoDB/Mongoose. Check out my projects page to see some of the projects I have worked on.
+        Newly minted full stack web developer. Front end fundementals in HTML, CSS, and JavaScript/TypeScript. Expereience with Bootstrap, Handlebars.js, and ReactJS. Back end experience with NodeJS, Express, Apollo/graphQL, MySQL/Sequelize, and MongoDB/Mongoose. Check out my projects page to see some of the projects I have worked on.
       </p>
       <p>
         My background prior to this in Mechanical Engineering where I conducted spacecraft fire safety research and learned to code with MATLAB. I am always eager and willing to learn!
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -7,6 +7,8 @@ export default function ProjectPage() {
   return (
     <main >
       <div>
+      <Container projectTitle='SLO Stone Properties' projectGithubLink='https://github.com/RCLobster/slo-stone-properties' projectLink='https://www.slostoneproperties.com/' projectImage='../assets/images/slo-stone.png'>
+      </Container>  
       <Container projectTitle='Mad Libs Generator' projectGithubLink='https://github.com/RCLobster/team-M-s-project-3' projectLink='https://team-m-s-project-3.onrender.com/' projectImage='../assets/images/madlibgen_home.png'>
       </Container>  
       <Container projectTitle='Breaking Bread' projectGithubLink='https://github.com/falafelrapper/breaking-bread' projectLink='https://agile-cove-66310-81bc545fcfa7.herokuapp.com/' projectImage='../assets/images/breaking-bread.png'>
